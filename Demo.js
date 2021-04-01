a408e81f0aa07d5698ea59a4018bc9dd075619f9
SumantChauhan-at-594407602270 <sumant.chauhan@tecnovos.com>
2021-04-01 15:56:15 +0530

    updated

--- Demo.js
+++ Demo.js
@@ -5,7 +5,7 @@ const fibonacci = (number) => {
     n2 = 1,
     nextTerm;
   for (let i = 1; i <= number; i++) {
-    console.log(n1);
+    log(n1);
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
@@ -17,7 +17,7 @@ const fibonacci = (number) => {
 const checkPrime = (number) => {
   let isPrime = true;
   if (number === 1) {
-    console.log("1 is neither prime nor composite number.");
+    log("1 is neither prime nor composite number.");
   } else if (number > 1) {
     for (let i = 2; i < number; i++) {
       if (number % i == 0) {
@@ -26,12 +26,12 @@ const checkPrime = (number) => {
       }
     }
     if (isPrime) {
-      console.log(`${number} is a prime number`);
+      log(`${number} is a prime number`);
     } else {
-      console.log(`${number} is a not prime number`);
+      log(`${number} is a not prime number`);
     }
   } else {
-    console.log("The number is not a prime number.");
+    log("The number is not a prime number.");
   }
 };
 
@@ -39,22 +39,22 @@ const checkPrime = (number) => {
 
 const factorial = (number) => {
   if (number < 0) {
-    console.log("Error! Factorial for negative number does not exist.");
+    log("Error! Factorial for negative number does not exist.");
   } else if (number === 0) {
-    console.log(`The factorial of ${number} is 1.`);
+    log(`The factorial of ${number} is 1.`);
   } else {
     let fact = 1;
     for (i = 1; i <= number; i++) {
       fact *= i;
     }
-    console.log(`The factorial of ${number} is ${fact}.`);
+    log(`The factorial of ${number} is ${fact}.`);
   }
 };
 
 // factorial(5); //The factorial of 5 is 120.
 
 const allPrime = (min, max) => {
-  console.log(`The prime numbers between ${min} and ${max} are:`);
+  log(`The prime numbers between ${min} and ${max} are:`);
   for (let i = min; i <= max; i++) {
     let flag = 0;
     for (let j = 2; j < i; j++) {
@@ -64,7 +64,7 @@ const allPrime = (min, max) => {
       }
     }
     if (i > 1 && flag == 0) {
-      console.log(i);
+      log(i);
     }
   }
 };
@@ -81,9 +81,9 @@ const armstrong = (number) => {
     temp = parseInt(temp / 10);
   }
   if (sum == number) {
-    console.log(`${number} is an Armstrong number`);
+    log(`${number} is an Armstrong number`);
   } else {
-    console.log(`${number} is not an Armstrong number.`);
+    log(`${number} is not an Armstrong number.`);
   }
 };
 
@@ -101,7 +101,7 @@ const allArmstrong = (min, max) => {
     }
 
     if (sum == i) {
-      console.log(i);
+      log(i);
     }
   }
 };
@@ -114,11 +114,11 @@ function checkPalindrome(string) {
   const len = string.length;
   for (let i = 0; i < len / 2; i++) {
     if (string[i] !== string[len - 1 - i]) {
-      console.log("It is not a palindrome");
+      log("It is not a palindrome");
       return "It is not a palindrome";
     }
   }
-  console.log("It is a palindrome");
+  log("It is a palindrome");
   return "It is a palindrome";
 }
 
@@ -141,7 +141,7 @@ let arr2 = [11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 23];
 function findMissing(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i + 1] - arr[i] !== 1) {
-      console.log(arr[i] + 1);
+      log(arr[i] + 1);
     }
   }
 }
@@ -156,7 +156,7 @@ function findPair(arr) {
   for (let i = 0; i < arr.length - 2; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] === constant) {
-        console.log(arr[i], arr[j]);
+        log(arr[i], arr[j]);
       }
     }
   }
@@ -180,7 +180,7 @@ function sortArray(arr) {
       }
     }
   }
-  console.log(arr);
+  log(arr);
 }
 
 // sortArray(unsortedArray);
@@ -195,7 +195,7 @@ const insertionSort = (nums) => {
     }
     nums[j + 1] = temp;
   }
-  console.log(nums);
+  log(nums);
   return nums;
 };
 
@@ -216,7 +216,7 @@ let selectionSort = (arr) => {
       arr[min] = tmp;
     }
   }
-  console.log(arr);
+  log(arr);
   return arr;
 };
 
@@ -233,7 +233,7 @@ function secondLargest(arr) {
       secondLargest = arr[i];
     }
   }
-  console.log(secondLargest);
+  log(secondLargest);
   return secondLargest;
 }
 
@@ -317,3 +317,172 @@ var array1 = [1, 2, 3];
 var array2 = [2, 30, 1];
 
 // merge_array(array1, array2);
+
+const check_miss = (n) => {
+  let temp = Array(Math.max(...n)).fill(0);
+
+  n.forEach((item) => (temp[item] = 1));
+
+  const missing_items = temp
+    .map((item, index) => (item === 0 ? index : -1))
+    .filter((item) => item !== -1);
+
+  console.log(missing_items);
+};
+
+n = [5, 4, 2, 1, 10, 20, 0];
+// check_miss(n);       //[ 3,  6,  7,  8,  9, 11,12, 13, 14, 15, 16, 17,18, 19]
+
+function findMissingNumbers(arr) {
+  var sparse = arr.reduce((sparse, i) => ((sparse[i] = 1), sparse), []);
+  return [...sparse.keys()].filter((i) => i && !sparse[i]);
+}
+
+var someArr = [2, 5, 3, 1, 4, 7, 10, 15];
+var sResult = findMissingNumbers(someArr);
+// console.log(sResult);      //[
+//   6,  8,  9, 11,
+//  12, 13, 14
+// ]
+
+const countOcurence = (str) => {
+  let obj = {};
+  for (let s of str) {
+    if (!obj[s]) {
+      obj[s] = 1;
+    } else {
+      obj[s] = obj[s] + 1;
+    }
+  }
+  return obj;
+};
+
+let Occurence = countOcurence("my name is atul kumar srivastava");
+// log("Occurence", Occurence);
+
+const arr = [4, 2, -1, 0, 3, 9, 1, -5, 10, -20, 5];
+const findSmallestMissing = (arr = []) => {
+  let count = 1;
+  if (!arr.length) {
+    return count;
+  }
+  while (arr.indexOf(count) !== -1) {
+    count++;
+  }
+  return count;
+};
+// log(findSmallestMissing(arr));
+
+function getCommonItems(array1, array2) {
+  var common = []; // Initialize array to contain common items
+
+  for (var i = 0; i < array1.length; i++) {
+    for (var j = 0; j < array2.length; j++) {
+      if (array1[i] == array2[j]) {
+        // If item is present in both arrays
+        common.push(array1[i]); // Push to common array
+      }
+    }
+  }
+
+  return common; // Return the common items
+}
+
+var array1 = [1, 4, 5, 6, 8];
+var array2 = [1, 2, 3, 8, 9];
+
+// Get common items of array1, array2
+var commonItemList = getCommonItems(array1, array2);
+
+// console.log(commonItemList);
+
+const removeDuplicateItem = (arr) => {
+  // let unique = [...new Map(arr.map((item) => [item.itemName, item])).values()];
+  // return unique;
+  let array = {};
+  let unique = [];
+  arr.forEach((item) => {
+    if (!array[item.itemName]) {
+      array[item.itemName] = true;
+      unique.push(item);
+    }
+  });
+  log(array);
+  return unique;
+};
+
+let originalItems = [
+  {
+    id: 1,
+    itemName: "AAAAAAAAAA",
+    quantity: 20,
+  },
+  {
+    id: 2,
+    itemName: "AAAAAAAAAA",
+    quantity: 20,
+  },
+  {
+    id: 3,
+    itemName: "BBBBBBBBBB",
+    quantity: 20,
+  },
+  {
+    id: 4,
+    itemName: "CCCCCCCCCCC",
+    quantity: 20,
+  },
+  {
+    id: 5,
+    itemName: "BBBBBBBBBB",
+    quantity: 20,
+  },
+  {
+    id: 6,
+    itemName: "BBBBBBBBBB",
+    quantity: 20,
+  },
+  {
+    id: 7,
+    itemName: "AAAAAAAAAA",
+    quantity: 30,
+  },
+];
+
+// let uniqueItems = removeDuplicateItem(originalItems);
+// log("uniqueItems", uniqueItems);
+
+const removeDuplicateItemUsingFilter = (arr) => {
+  let array = {};
+  let unique = arr.filter((item) => {
+    if (!array[item.itemName]) {
+      array[item.itemName] = true;
+      return item;
+    }
+  });
+  return unique;
+};
+
+// let uniqueObj = removeDuplicateItemUsingFilter(originalItems);
+// log(uniqueObj);
+
+const firstMissingPositive = function (nums) {
+  if (nums.length < 1) {
+    return 1;
+  }
+  let obj = {};
+  for (let num of nums) {
+    if (num > 0) obj[num] = true;
+  }
+  let missing = 1;
+  for (missing; missing <= nums.length; missing++) {
+    if (!obj[missing]) {
+      return missing;
+    }
+  }
+  return missing;
+};
+
+let mixedNum = [10, -2, 6, 1, 9, -20, 30, 8, 2, 3, 15, 4, 5];
+let firstMissingPositiveRes = firstMissingPositive(mixedNum);
+log(firstMissingPositiveRes);
